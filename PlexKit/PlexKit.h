5195f53ceb1a083d06173d04081d16cbd90bd6fa
Till Hagger <till.hagger@gmail.com>
2013-06-26 14:24:18 +0200

    Initial import

---
+++ PlexKit/PlexKit.h
@@ -0,0 +1,13 @@
+//
+//  PlexKit.h
+//  PlexKit
+//
+//  Created by Till Hagger on 6/26/13.
+//  Copyright (c) 2013 Till Hagger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlexKit : NSObject
+
+@end
---
+++ PlexKit/PlexKit.m
@@ -0,0 +1,13 @@
+//
+//  PlexKit.m
+//  PlexKit
+//
+//  Created by Till Hagger on 6/26/13.
+//  Copyright (c) 2013 Till Hagger. All rights reserved.
+//
+
+#import "PlexKit.h"
+
+@implementation PlexKit
+
+@end
